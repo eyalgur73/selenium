221cee6029ddc0b0c9201806e29dfc13392718a8
eyalgur73 <52457146+eyalgur73@users.noreply.github.com>
2019-07-03 20:03:18 +0300

    prepare the csv for writing all the links

---
+++ SeleniumExample.py
@@ -0,0 +1,27 @@
+
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+import unittest
+import csv
+
+browser = webdriver.Chrome()
+
+browser.get('http://google.com')
+
+element = browser.find_element_by_name('q')
+
+element.send_keys("site:co.il QA Manager")
+
+element.send_keys(Keys.RETURN)
+
+
+
+links = []
+with open('SitesList.csv') as csv_file:
+    file_writer = csv.writer(csv_file)
+for row in file_writer:
+    links.append(row)
+
+browser.quit()
+
+
